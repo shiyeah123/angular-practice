7459ce22676d1f296f7c40b403e54fbab0d25095
SHI <shiyeah123@foxmail.com>
2019-11-06 17:49:55 +0800

    路由模块

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ValidateFormComponent } from './validate-form/validate-form.component';
+import { LoginFormComponent } from './login-from/login-from.component';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+     path: 'validate',
+     component: ValidateFormComponent
+  },
+  {
+     path: 'login',
+     component: LoginFormComponent
+  },
+  {
+     path: '',
+     component: AppComponent
+
+  },
+  {
+     path: '**',
+     component: PageNotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import { ValidateFormComponent } from './validate-form/validate-form.component';
+import { LoginFormComponent } from './login-from/login-from.component';
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 
 registerLocaleData(zh);
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValidateFormComponent
+      ValidateFormComponent,
+      LoginFormComponent,
+      PageNotFoundComponent,
+
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       NgZorroAntdModule,
       FormsModule,
+      ReactiveFormsModule,
       HttpClientModule,
       BrowserAnimationsModule
    ],
